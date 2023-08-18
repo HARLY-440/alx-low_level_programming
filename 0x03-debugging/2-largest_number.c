98c1351488b38c2682e84deb737ad4c5c007d33b
HAMZA FARHI <yasnesan@gmail.com>
2023-08-17 18:02:24 -0700

    hi

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+int largest_number(int a, int b, int c)
+{
+int largest;
+
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (b > c && b > a)
+	{
+		largest =b;
+	}
+	else if (c > b)
+	{
+		largest = c;
+	}
+	else
+	{
+		largest = b;
+	}
+
+return (largest);
+}
